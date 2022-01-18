5326f37c7dd502bb0c9bf0143d232c9e33829f0f
Zafar <naveedulz@gmail.com>
2022-01-18 12:16:29 +0100

    ok

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,60 +6,61 @@
 
 <div class="sidebar-container mt-10 pt-10">
     <div class="sidebar-logo">
-      Back Office
+        Back Office
     </div>
     <ul class="sidebar-navigation">
-      <li class="header">Create Formulaire</li>
-      <li>
-        <a href="{{route('blog.create')}}">
-          <i class="fa fa-home" aria-hidden="true"></i> Blog -form
-        </a>
-      </li>
-      <li>
-        <a href="{{route('portfolio.create')}}">
-          <i class="fa fa-tachometer" aria-hidden="true"></i> Portfolio -form
-        </a>
-      </li>
+        <li class="header">Create Formulaire</li>
+        <li>
+            <a href="{{route('blog.create')}}">
+                <i class="fa fa-home" aria-hidden="true"></i> Blog -form
+            </a>
+        </li>
+        <li>
+            <a href="{{route('portfolio.create')}}">
+                <i class="fa fa-tachometer" aria-hidden="true"></i> Portfolio -form
+            </a>
+        </li>
 
     </ul>
     <ul class="sidebar-navigation">
         <li class="header">Affichage</li>
         <li>
-          <a href="{{route('blog.affichage')}}">
-            <i class="fa fa-home" aria-hidden="true"></i> Blog
-          </a>
+            <a href="{{route('blog.affichage')}}">
+                <i class="fa fa-home" aria-hidden="true"></i> Blog
+            </a>
         </li>
         <li>
-          <a href="{{route('portfolio.affichage')}}">
-            <i class="fa fa-tachometer" aria-hidden="true"></i> Portfolio
-          </a>
+            <a href="{{route('portfolio.affichage')}}">
+                <i class="fa fa-tachometer" aria-hidden="true"></i> Portfolio
+            </a>
         </li>
         <li class="header">Another Menu</li>
         <li>
-          <a href="#">
-            <i class="fa fa-users" aria-hidden="true"></i> Friends
-          </a>
+            <a href="#">
+                <i class="fa fa-users" aria-hidden="true"></i> Friends
+            </a>
         </li>
         <li>
-          <a href="#">
-            <i class="fa fa-cog" aria-hidden="true"></i> Settings
-          </a>
+            <a href="#">
+                <i class="fa fa-cog" aria-hidden="true"></i> Settings
+            </a>
         </li>
         <li>
-          <a href="#">
-            <i class="fa fa-info-circle" aria-hidden="true"></i> Information
-          </a>
+            <a href="#">
+                <i class="fa fa-info-circle" aria-hidden="true"></i> Information
+            </a>
         </li>
-      </ul>
-  </div>
-
-  <div class="content-container">
-
-
-    {{-- table --}}
-
+    </ul>
+</div>
+<div>
     <h2 class="sub-header">Dashboard</h2>
-              <div class="table-responsive">
+</div>
+{{-- <div class="content-container"> --}}
+
+
+{{-- table --}}
+
+{{-- <div class="table-responsive">
                   <table class="table table-striped">
                       <thead>
                           <tr class="table-info">
@@ -86,10 +87,10 @@
                           </tr>
                       </tbody>
                   </table>
-              </div>
+              </div> --}}
 
-              {{-- blog --}}
-              <h3>BLOG :</h3>
+{{-- blog --}}
+{{-- <h3>BLOG :</h3>
               <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
@@ -105,19 +106,19 @@
                         @forelse ($afficheBlog as $item)
                         <tr>
                             <td>{{ $item->id }}</td>
-                            <td>{{ $item->titre }}</td>
-                            <td><img src="{{ $item->image }}" style="height:50px; width:50px;" alt=""></td>
-                            <td>{{ $item->description }}</td>
-                            <td  class="df">
-                            <a href="{{route('blog.show', $item->id)}}" class="btn btn vert">Show</a>
-                            <a href="{{route('blog.edit', $item->id)}}" class="btn btn orange edit">Edit</a>
-                            <form action="{{ route("blog.destroy", $item->id) }}" method="POST">
-                                @csrf
-                            @method('DELETE')
-                                <button type="submit" class="btn rouge">Delete</button>
-                                {{-- <button type="submit" class="btn vert">Show</button>
+<td>{{ $item->titre }}</td>
+<td><img src="{{ $item->image }}" style="height:50px; width:50px;" alt=""></td>
+<td>{{ $item->description }}</td>
+<td class="df">
+    <a href="{{route('blog.show', $item->id)}}" class="btn btn vert">Show</a>
+    <a href="{{route('blog.edit', $item->id)}}" class="btn btn orange edit">Edit</a>
+    <form action="{{ route("blog.destroy", $item->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn rouge">Delete</button> --}}
+        {{-- <button type="submit" class="btn vert">Show</button>
                                 <button type="submit" class="btn orange">Edit</button> --}}
-                              </td>
+        {{-- </td>
                             </form>
 
                         </tr>
@@ -130,17 +131,17 @@
                             <td>Vide</td>
                             <td>Vide</td>
                             <td>
-                                <button type="submit" class="btn rouge">Delete</button>
-                                {{-- <button type="submit" class="btn rouge">show</button> --}}
-                            </td>
+                                <button type="submit" class="btn rouge">Delete</button> --}}
+        {{-- <button type="submit" class="btn rouge">show</button> --}}
+        {{-- </td>
                         </tr>
                         @endforelse
                     </tbody>
                 </table>
-            </div>
+            </div> --}}
 
-            {{-- portfolio --}}
-            <h3>PORTFOLIO :</h3>
+        {{-- portfolio --}}
+        {{-- <h3>PORTFOLIO :</h3>
             <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
@@ -154,18 +155,19 @@
                     <tbody>
                         @forelse ($affichePortfolio as $item)
                         <tr>
-                            <td>{{ $item->id }}</td>
-                            <td>{{ $item->titre }}</td>
-                            <td>{{ $item->description }}</td>
-                            <td  class="df">
-                            <a href="{{route('portfolio.show', $item->id)}}" class="btn btn vert">Show</a>
-                            <a href="{{route('portfolio.edit', $item->id)}}" class="btn btn orange edit">Edit</a>
-                            <form action="{{ route("portfolio.destroy", $item->id) }}" method="POST">
-                                @csrf
-                            @method("DELETE")
-                                <button type="submit" class="btn rouge">Delete</button>
-                                {{-- <button type="submit" class="btn rouge">show</button> --}}
-                            </td>
+                            <td>{{ $item->id }}
+</td>
+<td>{{ $item->titre }}</td>
+<td>{{ $item->description }}</td>
+<td class="df">
+    <a href="{{route('portfolio.show', $item->id)}}" class="btn btn vert">Show</a>
+    <a href="{{route('portfolio.edit', $item->id)}}" class="btn btn orange edit">Edit</a>
+    <form action="{{ route("portfolio.destroy", $item->id) }}" method="POST">
+        @csrf
+        @method("DELETE")
+        <button type="submit" class="btn rouge">Delete</button> --}}
+        {{-- <button type="submit" class="btn rouge">show</button> --}}
+        {{-- </td>
                             </form>
                         </tr>
                         @empty
@@ -183,7 +185,7 @@
                         @endforelse
                     </tbody>
                 </table>
-            </div>
-  </div>
+            </div> --}}
+        {{-- </div> --}}
 
-@endsection
+        @endsection
